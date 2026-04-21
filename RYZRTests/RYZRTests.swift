58c572cb7d4c6b3fd30e1ba4f02c94c90826505b
Nicholas Coppola <nicholas.coppola67@gmail.com>
2026-04-21 13:19:34 -0400

    Initial Commit

---
+++ RYZRTests/RYZRTests.swift
@@ -0,0 +1,19 @@
+//
+//  RYZRTests.swift
+//  RYZRTests
+//
+//  Created by Nicholas Coppola on 4/21/26.
+//
+
+import Testing
+@testable import RYZR
+
+struct RYZRTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
